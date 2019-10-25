6546ce2c01f20d0db8f65592ad717d5c3c288cec
mmiesch <miesch@ucar.edu>
2019-10-25 16:50:36 +0000

    more bugfixes on build script

--- build_intel_dev_container.sh
+++ build_intel_dev_container.sh
@@ -13,6 +13,7 @@ echo "Building Intel development container "
 ../hpc-container-maker/hpccm.py --recipe ${CH_NAME}.py --format docker > Dockerfile.${CH_NAME}
 
 # build the Docker image
+cd context
 sudo docker image build --no-cache -f Dockerfile.${CH_NAME} -t jedi-${CH_NAME} .
 
 # save the Docker image to a file:
@@ -20,8 +21,7 @@ mkdir -p containers
 sudo docker save jedi-${CH_NAME}:latest | gzip > containers/docker-${CH_NAME}.tar.gz
 
 # build the Charliecloud image
-ch-build -t ${CH_NAME} -f Dockerfile.intel-impi-dev .
-ch-builder2tar ${CH_NAME} containers
+ch-builder2tar jedi-${CH_NAME} containers
 
 # Optionally copy to amazon S3
 ans=''
@@ -34,7 +34,7 @@ done
 if [[ $ans == y ]] ; then
   echo "Sending to Amazon S3" 
   aws s3 cp containers/docker-${CH_NAME}.tar.gz s3://privatecontainers/docker-${CH_NAME}.tar.gz
-  aws s3 cp containers/${CH_NAME}.tar.gz s3://privatecontainers/ch-${CH_NAME}.tar.gz
+  aws s3 cp containers/jedi-${CH_NAME}.tar.gz s3://privatecontainers/ch-${CH_NAME}.tar.gz
 else
   echo "Not sending to Amazon S3" 
 fi
