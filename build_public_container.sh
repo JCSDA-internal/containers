614997662778c61615381322ed2b3e8996caad24
mmiesch <miesch@ucar.edu>
2019-12-03 19:57:22 +0000

    tweaks to build script

--- build_public_container.sh
+++ build_public_container.sh
@@ -25,26 +25,40 @@ fi
 # Stop if anything goes wrong
 set -e
 
+# may need to use sudo unless you're running as root
+export USE_SUDO=${USE_SUDO:-"y"}
+[[ $USE_SUDO =~ [yYtT] ]] && export SUDO="sudo" || unset SUDO
+
 export CNAME=${1:-"gnu-openmpi-dev"}
+
+echo "=============================================================="
+echo "   Building Docker Image" ${CNAME}
+echo "=============================================================="
+
+mkdir -p context
+cd context
+ln -sf ../Dockerfile.${CNAME} .
+$SUDO docker image build --no-cache --pull -t ch-${CNAME} -f Dockerfile.${CNAME} .
+cd ..
 
 echo "=============================================================="
 echo "   Building Charliecloud Image" ${CNAME}
 echo "=============================================================="
 
-get_ans "Build Singularity image?"
+get_ans "Build Charliecloud image?"
 
 if [[ $ans == y ]] ; then
     echo "Building Charliecloud image"
-    docker image build --no-cache --pull -t ${CNAME} -f Dockerfile.${CNAME} .
     mkdir -p containers
-    ch-builder2tar ${CNAME} containers
+    $SUDO ch-builder2tar ch-${CNAME} containers
 
     # Optionally copy to amazon S3
-    get_ans "Copy Charliecloud image to S3?"
+    get_ans "Push Charliecloud image to S3?"
     if [[ $ans == y ]] ; then
 	echo "Sending to Amazon S3" 
-	aws s3 mv s3://data.jcsda.org/containers/ch-jedi-${CNAME}.tar.gz s3://data.jcsda.org/containers/ch-jedi-${CNAME}-revert.tar.gz
-	aws s3 cp containers/${CNAME}.tar.gz s3://data.jcsda.org/containers/ch-jedi-${CNAME}.tar.gz
+        [[ $("aws s3 ls s3://data.jcsda.org/containers/") ]] && \
+	   aws s3 mv s3://data.jcsda.org/containers/ch-jedi-${CNAME}.tar.gz s3://data.jcsda.org/containers/ch-jedi-${CNAME}-revert.tar.gz
+	aws s3 cp containers/ch-${CNAME}.tar.gz s3://data.jcsda.org/containers/ch-jedi-${CNAME}.tar.gz
     else
 	echo "Not sending to Amazon S3" 
     fi
@@ -59,20 +73,29 @@ echo "=============================================================="
 get_ans "Build Singularity image?"
 
 if [[ $ans == y ]] ; then 
-   echo "Building Singularity image"
+
    mkdir -p containers
    cd containers
-   #sudo singularity build jedi-$name.sif docker://jcsda/docker-$name:latest
-   sudo singularity build jedi-$name.sif Singularity.$CNAME
-   singularity sign jedi-$name.sif
 
    # Optionally push to sylabs cloud
+   get_ans "Make backup image on sylabs cloud?"
+   if [[ $ans == y ]] ; then
+       echo "Creating backup image"
+       singularity pull --force library://jcsda/public/jedi-${CNAME}:latest
+       singularity push jedi-${CNAME}_latest.sif library://jcsda/public/jedi-${CNAME}:revert
+       rm jedi-${CNAME}_latest.sif
+   else
+       echo "Not pushing to sylabs cloud"
+   fi
+
+   echo "Building Singularity image"
+   $SUDO singularity build jedi-$CNAME.sif ../Singularity.${CNAME}
+
    get_ans "Push singularity image to sylabs cloud?"
    if [[ $ans == y ]] ; then
        echo "Pushing to sylabs cloud"
-       singularity push jedi-$name.sif library://jcsda/public/jedi-$name:latest
-   else
-       echo "Not pushing to sylabs cloud"
+       singularity sign jedi-$CNAME.sif
+       singularity push jedi-$CNAME.sif library://jcsda/public/jedi-$CNAME:latest
    fi
 else
    echo "Not building Singularity image"
