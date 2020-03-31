90ac9ce9c652678803cb54620125d99928ccbafd
mmiesch <miesch@ucar.edu>
2020-03-31 23:39:34 +0000

    working without fv3-bundle

--- intel19-impi-hpc-app.py
+++ intel19-impi-hpc-app.py
@@ -82,13 +82,16 @@ Stage0 += shell(commands=['apt-get update',
 Stage0 += environment(variables={'LANG':'en_US.UTF-8','LANGUAGE':'en_US:en'})
 
 # set environment variables for jedi-stack and jedi build
-Stage0 += environment(variables={'NETCDF':'/usr/local/jedi',
-                                 'NETCDF_ROOT':'/usr/local/jedi',
-                                 'PNETCDF':'/usr/local/jedi',
-                                 'HDF5_ROOT':'/usr/local/jedi',
-                                 'PIO':'/usr/local/jedi',
-                                 'BOOST_ROOT':'/usr/local/jedi',
-                                 'EIGEN3_INCLUDE_DIR':'/usr/local/jedi',
+Stage0 += environment(variables={'NETCDF':'/opt/jedi',
+                                 'NETCDF_ROOT':'/opt/jedi',
+                                 'PNETCDF':'/opt/jedi',
+                                 'HDF5_ROOT':'/opt/jedi',
+                                 'PIO':'/opt/jedi',
+                                 'BOOST_ROOT':'/opt/jedi',
+                                 'EIGEN3_INCLUDE_DIR':'/opt/jedi/include',
+                                 'PATH':'/opt/jedi/bin:$PATH',
+                                 'LD_LIBRARY_PATH':'/opt/jedi/lib:$LD_LIBRARY_PATH',
+                                 'LIBRARY_PATH':'/opt/jedi/lib:$LIBRARY_PATH',
                                  'SERIAL_CC':'icc',
                                  'SERIAL_CXX':'icpc',
                                  'SERIAL_FC':'ifort',
@@ -115,9 +118,10 @@ Stage0 += shell(commands=['cd /root',
 Stage0 += shell(commands=['echo "export FC=mpiifort" >> /etc/bash.bashrc',
     'echo "export CC=mpiicc" >> /etc/bash.bashrc',
     'echo "export CXX=mpiicpc" >> /etc/bash.bashrc',
-    'echo "export PATH=/usr/local/bin:$PATH" >> /etc/bash.bashrc',
-    'echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH" >> /etc/bash.bashrc',
-    'echo "export LIBRARY_PATH=/usr/local/lib:$LIBRARY_PATH" >> /etc/bash.bashrc',
+    'echo "export PATH=/usr/local/bin:/opt/jedi/bin:$PATH" >> /etc/bash.bashrc',
+    'echo "export LD_LIBRARY_PATH=/usr/local/lib:/opt/jedi/lib:$LD_LIBRARY_PATH" >> /etc/bash.bashrc',
+    'echo "export LIBRARY_PATH=/usr/local/lib:/opt/jedi/lib:$LIBRARY_PATH" >> /etc/bash.bashrc',
+    'echo "export CPATH=/usr/local/include:/opt/jedi/include:$CPATH" >> /etc/bash.bashrc',
     'echo "export PYTHONPATH=/usr/local/lib:$PYTHONPATH" >> /etc/bash.bashrc'])
 
 # this appears to be needed to avoid errors in subsequent stages
@@ -131,37 +135,37 @@ Stage0 += copy(src='ssh-key/github_academy_rsa', dest='/root/github_academy_rsa'
 # generating a docker SHELL block
 Stage0 += shell(commands=['DOCKERSHELL BASH'])
 
-Stage0 += shell(commands=[
-    'source /opt/intel/compilers_and_libraries/linux/bin/compilervars.sh intel64',
-    'mkdir -p /root/.ssh',
-    'mv /root/github_academy_rsa /root/.ssh/github_academy_rsa',
-    'eval "$(ssh-agent -s)"',
-    'ssh-add /root/.ssh/github_academy_rsa',
-    'export CC=mpiicc','export CXX=mpiicpc','export FC=mpiifort',
-    'ssh -T -o "StrictHostKeyChecking=no" git@github.com; mkdir -p /jedi',
-    'cd /jedi',
-    'git clone git@github.com:jcsda/fv3-bundle.git',
-    'cd /jedi/fv3-bundle',
-    'git clone git@github.com:jcsda/fckit.git -b develop',
-    'git clone git@github.com:jcsda/crtm.git -b develop',
-    'git clone git@github.com:jcsda/saber.git -b develop',
-    'git clone git@github.com:jcsda/oops.git -b develop',
-    'git clone git@github.com:jcsda/ioda.git -b develop',
-    'git clone git@github.com:jcsda/ufo.git -b develop',
-    'git clone git@github.com:jcsda/fms.git -b dev/master-ecbuild',
-    'git clone git@github.com:jcsda/femps.git -b develop',
-    'git clone git@github.com:jcsda/fv3-jedi-linearmodel.git -b develop fv3-jedi-lm',
-    'git clone git@github.com:jcsda/fv3-jedi.git -b develop',
-    'mkdir -p /jedi/build','cd /jedi/build',
-    'ecbuild --build=Release ../fv3-bundle',
-    'make -j4', 'chmod -R 777 /jedi',
-    'rm /root/.ssh/github_academy_rsa'])
-
+#Stage0 += shell(commands=[
+#    'source /opt/intel/compilers_and_libraries/linux/bin/compilervars.sh intel64',
+#    'mkdir -p /root/.ssh',
+#    'mv /root/github_academy_rsa /root/.ssh/github_academy_rsa',
+#    'eval "$(ssh-agent -s)"',
+#    'ssh-add /root/.ssh/github_academy_rsa',
+#    'export CC=mpiicc','export CXX=mpiicpc','export FC=mpiifort',
+#    'ssh -T -o "StrictHostKeyChecking=no" git@github.com; mkdir -p /opt/jedi/fv3-bundle',
+#    'cd /opt/jedi/fv3-bundle',
+#    'git clone git@github.com:jcsda/fv3-bundle.git',
+#    'cd /opt/jedi/fv3-bundle/fv3-bundle',
+#    'git clone git@github.com:jcsda/fckit.git -b develop',
+#    'git clone git@github.com:jcsda/crtm.git -b develop',
+#    'git clone git@github.com:jcsda/saber.git -b develop',
+#    'git clone git@github.com:jcsda/oops.git -b develop',
+#    'git clone git@github.com:jcsda/ioda.git -b develop',
+#    'git clone git@github.com:jcsda/ufo.git -b develop',
+#    'git clone git@github.com:jcsda/fms.git -b dev/master-ecbuild',
+#    'git clone git@github.com:jcsda/femps.git -b develop',
+#    'git clone git@github.com:jcsda/fv3-jedi-linearmodel.git -b develop fv3-jedi-lm',
+#    'git clone git@github.com:jcsda/fv3-jedi.git -b develop',
+#    'mkdir -p /opt/jedi/fv3-bundle/build','cd /opt/jedi/fv3-bundle/build',
+#    'ecbuild --build=Release ../fv3-bundle',
+#    'make -j4', 'chmod -R 777 /opt/jedi/fv3-bundle',
+#    'rm /root/.ssh/github_academy_rsa'])
+#
 # Add hello world program for testing MPI configuration on different platforms
 Stage0 += copy(src='./hello_world_mpi.c', dest='/root/jedi/hello_world_mpi.c')
 Stage0 += shell(commands=['export COMPILERVARS_ARCHITECTURE=intel64',
                       '. /opt/intel/compilers_and_libraries/linux/bin/compilervars.sh',
-                      'cd /root/jedi','mpiicc hello_world_mpi.c -o /usr/local/bin/hello_world_mpi -lstdc++'])
+                      'cd /root/jedi','mpiicc hello_world_mpi.c -o /opt/jedi/bin/hello_world_mpi -lstdc++'])
 
 #==============================================================================
 # Second stage: Runtime
@@ -176,44 +180,43 @@ Stage1 += pp.runtime()
 Stage1 += kn.runtime()
 Stage1 += x.runtime()
 Stage1 += u.runtime()
-Stage1 += copy(_from='build', src='/usr/local/jedi', dest='/usr/local/jedi')
-Stage1 += copy(_from='build', src='/jedi/build', dest='/jedi/build')
 
 # set some environment variables for bash users
-Stage1 += shell(commands=['echo "export PATH=/usr/local/bin:$PATH" >> /etc/bash.bashrc',
-    'echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH" >> /etc/bash.bashrc',
-    'echo "export LIBRARY_PATH=/usr/local/lib:$LIBRARY_PATH" >> /etc/bash.bashrc',
+Stage1 += shell(commands=['echo "export PATH=/usr/local/bin:/usr/local/jedi/bin:$PATH" >> /etc/bash.bashrc',
+    'echo "export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/jedi/lib:$LD_LIBRARY_PATH" >> /etc/bash.bashrc',
+    'echo "export LIBRARY_PATH=/usr/local/lib:/usr/local/jedi/lib:$LIBRARY_PATH" >> /etc/bash.bashrc',
+    'echo "source /opt/intel/psxe_runtime_2020/linux/bin/psxevars.sh" >> /etc/bash.bashrc',
     'echo "export PYTHONPATH=/usr/local/lib:$PYTHONPATH" >> /etc/bash.bashrc'])
 
 # install intel Parallel Studio runtime libraries
 # The hppcm building blocks are not working
-##Stage1 += intel_mpi(eula=True)
-##Stage1 += mkl(eula=True)
 #Stage1 += intel_psxe_runtime(eula=True,daal=False,ipp=False,tbb=False,version='2020.0.008')
 Stage1 += apt_get(ospackages=['apt-transport-https','ca-certificates','gcc',
           'man-db','openssh-client'])
-#Stage1 += shell(commands=['mkdir -p /root/tmp','cd /root/tmp',
-#    'wget  https://apt.repos.intel.com/2020/GPG-PUB-KEY-INTEL-PSXE-RUNTIME-2020',
-#    'apt-key add GPG-PUB-KEY-INTEL-PSXE-RUNTIME-2020',
-#    'rm GPG-PUB-KEY-INTEL-PSXE-RUNTIME-2020',
-#    'echo "deb https://apt.repos.intel.com/2020 intel-psxe-runtime main" >> /etc/apt/sources.list.d/hpccm.list',
-#    'apt-get update -y',
-#    'DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends aptitude',
-#    "aptitude install -y --without-recommends -o Aptitude::ProblemResolver::SolutionCost='100*canceled-actions,200*removals' intel-icc-runtime=2020.0-8 intel-ifort-runtime=2020-8 intel-mkl-runtime=2020.0-8 intel-mpi-runtime=2020.0-8",'rm -rf /var/lib/apt/lists/*'])
-#
-#Stage1 += environment(variables={'LD_LIBRARY_PATH':'/opt/intel/psxe_runtime_2020.0.8/linux/daal/lib/intel64_lin:/opt/intel/psxe_runtime_2020.0.8/linux/compiler/lib/intel64_lin:/opt/intel/psxe_runtime_2020.0.8/linux/mkl/lib/intel64_lin:/opt/intel/psxe_runtime_2020.0.8/linux/tbb/lib/intel64/gcc4.8:/opt/intel/psxe_runtime_2020.0.8/linux/ipp/lib/intel64:/opt/intel/psxe_runtime_2020.0.8/linux/ipp/../tbb/lib/intel64/gcc4.8:/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/libfabric/lib:/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/lib/release:/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/lib:/opt/intel/psxe_runtime_2020.0.8/linux/compiler/lib/intel64_lin:/usr/local/lib:/usr/local/ucx/lib:/usr/local/xpmem/lib:',
-#    'FI_PROVIDER_PATH':'/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/libfabric/lib/prov',
-#    'CLASSPATH':'/opt/intel/psxe_runtime_2020.0.8/linux/daal/lib/daal.jar:/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/lib/mpi.jar',
-#    'CPATH':'/opt/intel/psxe_runtime_2020.0.8/linux/daal/include:/opt/intel/psxe_runtime_2020.0.8/linux/mkl/include:/opt/intel/psxe_runtime_2020.0.8/linux/tbb/include:/opt/intel/psxe_runtime_2020.0.8/linux/ipp/include:/usr/local/xpmem/include:/usr/local/knem/include:',
-#    'NLSPATH':'/opt/intel/psxe_runtime_2020.0.8/linux/mkl/lib/intel64_lin/locale/%l_%t/%N:/opt/intel/psxe_runtime_2020.0.8/linux/compiler/lib/intel64_lin/locale/%l_%t/%N',
-#    'LIBRARY_PATH':'/opt/intel/psxe_runtime_2020.0.8/linux/daal/lib/intel64_lin:/opt/intel/psxe_runtime_2020.0.8/linux/compiler/lib/intel64_lin:/opt/intel/psxe_runtime_2020.0.8/linux/mkl/lib/intel64_lin:/opt/intel/psxe_runtime_2020.0.8/linux/tbb/lib/intel64/gcc4.8:/opt/intel/psxe_runtime_2020.0.8/linux/ipp/lib/intel64:/opt/intel/psxe_runtime_2020.0.8/linux/ipp/../tbb/lib/intel64/gcc4.8:/opt/intel/psxe_runtime_2020.0.8/linux/ipp/lib/intel64:/opt/intel/psxe_runtime_2020.0.8/linux/ipp/../tbb/lib/intel64/gcc4.8:/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/libfabric/lib:/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/lib/release:/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/lib:/opt/intel/psxe_runtime_2020.0.8/linux/compiler/lib/intel64_lin:/usr/local/lib:/usr/local/ucx/lib:/usr/local/xpmem/lib:',
-#    'MIC_LD_LIBRARY_PATH':'/opt/intel/psxe_runtime_2020.0.8/linux/compiler/lib/intel64_lin_mic',
-#    'PYTHONPATH':'/usr/local/lib:',
-#    'MANPATH':'/opt/intel/psxe_runtime_2020.0.8/linux/mpi/man:/usr/local/man:/usr/local/share/man:/usr/share/man',
-#    'PATH':'/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/libfabric/bin:/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/bin:/opt/intel/psxe_runtime_2020.0.8/linux/bin:/usr/local/bin:/usr/local/ucx/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-#    'PKG_CONFIG_PATH':'/opt/intel/psxe_runtime_2020.0.8/linux/mkl/bin/pkgconfig',
-#    'I_MPI_ROOT':'/opt/intel/psxe_runtime_2020.0.8/linux/mpi',
-#    'IPPROOT':'/opt/intel/psxe_runtime_2020.0.8/linux/ipp',
-#    'DAALROOT':'/opt/intel/psxe_runtime_2020.0.8/linux/daal',
-#    'MKLROOT':'/opt/intel/psxe_runtime_2020.0.8/linux/mkl',
-#    'TBBROOT':'/opt/intel/psxe_runtime_2020.0.8/linux/tbb'})
+Stage1 += shell(commands=['mkdir -p /root/tmp','cd /root/tmp',
+    'wget  https://apt.repos.intel.com/2020/GPG-PUB-KEY-INTEL-PSXE-RUNTIME-2020',
+    'apt-key add GPG-PUB-KEY-INTEL-PSXE-RUNTIME-2020',
+    'rm GPG-PUB-KEY-INTEL-PSXE-RUNTIME-2020',
+    'echo "deb https://apt.repos.intel.com/2020 intel-psxe-runtime main" >> /etc/apt/sources.list.d/hpccm.list',
+    'apt-get update -y',
+    'DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends aptitude',
+    "aptitude install -y --without-recommends -o Aptitude::ProblemResolver::SolutionCost='100*canceled-actions,200*removals' intel-icc-runtime=2020.0-8 intel-ifort-runtime=2020.0-8 intel-mkl-runtime=2020.0-8 intel-mpi-runtime=2020.0-8",'rm -rf /var/lib/apt/lists/*'])
+
+Stage1 += environment(variables={'LD_LIBRARY_PATH':'/opt/intel/psxe_runtime_2020.0.8/linux/daal/lib/intel64_lin:/opt/intel/psxe_runtime_2020.0.8/linux/compiler/lib/intel64_lin:/opt/intel/psxe_runtime_2020.0.8/linux/mkl/lib/intel64_lin:/opt/intel/psxe_runtime_2020.0.8/linux/tbb/lib/intel64/gcc4.8:/opt/intel/psxe_runtime_2020.0.8/linux/ipp/lib/intel64:/opt/intel/psxe_runtime_2020.0.8/linux/ipp/../tbb/lib/intel64/gcc4.8:/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/libfabric/lib:/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/lib/release:/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/lib:/opt/intel/psxe_runtime_2020.0.8/linux/compiler/lib/intel64_lin:/usr/local/lib:/usr/local/ucx/lib:/usr/local/xpmem/lib:',
+    'FI_PROVIDER_PATH':'/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/libfabric/lib/prov',
+    'CLASSPATH':'/opt/intel/psxe_runtime_2020.0.8/linux/daal/lib/daal.jar:/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/lib/mpi.jar',
+    'CPATH':'/opt/intel/psxe_runtime_2020.0.8/linux/daal/include:/opt/intel/psxe_runtime_2020.0.8/linux/mkl/include:/opt/intel/psxe_runtime_2020.0.8/linux/tbb/include:/opt/intel/psxe_runtime_2020.0.8/linux/ipp/include:/usr/local/xpmem/include:/usr/local/knem/include:',
+    'LIBRARY_PATH':'/opt/intel/psxe_runtime_2020.0.8/linux/daal/lib/intel64_lin:/opt/intel/psxe_runtime_2020.0.8/linux/compiler/lib/intel64_lin:/opt/intel/psxe_runtime_2020.0.8/linux/mkl/lib/intel64_lin:/opt/intel/psxe_runtime_2020.0.8/linux/tbb/lib/intel64/gcc4.8:/opt/intel/psxe_runtime_2020.0.8/linux/ipp/lib/intel64:/opt/intel/psxe_runtime_2020.0.8/linux/ipp/../tbb/lib/intel64/gcc4.8:/opt/intel/psxe_runtime_2020.0.8/linux/ipp/lib/intel64:/opt/intel/psxe_runtime_2020.0.8/linux/ipp/../tbb/lib/intel64/gcc4.8:/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/libfabric/lib:/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/lib/release:/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/lib:/opt/intel/psxe_runtime_2020.0.8/linux/compiler/lib/intel64_lin:/usr/local/lib:/usr/local/ucx/lib:/usr/local/xpmem/lib:',
+    'MIC_LD_LIBRARY_PATH':'/opt/intel/psxe_runtime_2020.0.8/linux/compiler/lib/intel64_lin_mic',
+    'PYTHONPATH':'/usr/local/lib:',
+    'MANPATH':'/opt/intel/psxe_runtime_2020.0.8/linux/mpi/man:/usr/local/man:/usr/local/share/man:/usr/share/man',
+    'PATH':'/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/libfabric/bin:/opt/intel/psxe_runtime_2020.0.8/linux/mpi/intel64/bin:/opt/intel/psxe_runtime_2020.0.8/linux/bin:/usr/local/bin:/usr/local/ucx/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    'PKG_CONFIG_PATH':'/opt/intel/psxe_runtime_2020.0.8/linux/mkl/bin/pkgconfig',
+    'I_MPI_ROOT':'/opt/intel/psxe_runtime_2020.0.8/linux/mpi',
+    'IPPROOT':'/opt/intel/psxe_runtime_2020.0.8/linux/ipp',
+    'DAALROOT':'/opt/intel/psxe_runtime_2020.0.8/linux/daal',
+    'MKLROOT':'/opt/intel/psxe_runtime_2020.0.8/linux/mkl',
+    'TBBROOT':'/opt/intel/psxe_runtime_2020.0.8/linux/tbb'})
+
+Stage1 += copy(_from='build', src='/opt/jedi', dest='/opt/jedi')
+
