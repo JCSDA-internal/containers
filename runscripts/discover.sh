30e10a14184d96d36f70d7544d0c57470cb50753
mmiesch <miesch@ucar.edu>
2020-04-10 14:58:08 -0600

    runscripts for S4 and Discover

---
+++ runscripts/discover.sh
@@ -0,0 +1,24 @@
+#!/usr/local/bin/bash
+# --mem-per-cpu=8192M
+#SBATCH --job-name=con1
+#SBATCH --ntasks=864
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1:00:00
+#SBATCH --mail-user=miesch@ucar.edu
+
+source /usr/share/modules/init/bash
+module purge
+module load comp/intel/20.0.0.166
+module load mpi/impi/20.0.0.166
+module load singularity
+module load core/jedi/singularityvars
+ulimit -s unlimited
+
+cd /discover/nobackup/mmiesch/runs/Hofx_benchmark/con
+
+JEDICON=/discover/nobackup/mmiesch
+JEDIBIN=/opt/jedi/fv3-bundle/build/bin
+
+mpirun -np 864 singularity exec --home=$PWD $JEDICON/jedi-intel19-impi-hpc-sandbox ${JEDIBIN}/fv3jedi_var.x Config/3dvar_new.yaml
+
+exit 0
