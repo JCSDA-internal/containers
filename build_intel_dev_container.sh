99e89126ec6a3737869c9039789c0577ab79f318
mmiesch <miesch@ucar.edu>
2020-06-22 20:20:16 +0000

    add update script, intel-dev singularity build

--- build_intel_dev_container.sh
+++ build_intel_dev_container.sh
@@ -29,7 +29,7 @@ if [[ $(echo ${CNAME} | cut -d- -f1) = "intel17" ]]; then
     export INTEL_TARBALL='./intel_tarballs/parallel_studio_xe_2017_update1.tgz'
     export INTEL_CONTEXT='./context17'
 elif [[ $(echo ${CNAME} | cut -d- -f1) = "intel19" ]]; then
-    export INTEL_TARBALL='./intel_tarballs/parallel_studio_xe_2020_update1_cluster_edition_online.tgz'
+    export INTEL_TARBALL='./intel_tarballs/parallel_studio_xe_2020_cluster_edition.tgz'
     export INTEL_CONTEXT='./context19'
 fi
 
@@ -83,6 +83,23 @@ if [[ $ans == y ]] ; then
   aws s3 cp containers/docker-${CNAME}.tar.gz s3://privatecontainers/docker-jedi-${CNAME}.tar.gz
 else
   echo "Not sending to Amazon S3"
+fi
+
+echo "=============================================================="
+echo "   Building Singularity Image"
+echo "=============================================================="
+# Optionally build the Singularity image
+get_ans "Build Singularity image?"
+if [[ $ans == y ]] ; then
+    echo "Building Singularity image"
+    rm -f singularity_build.log
+    sudo singularity build containers/jedi-${CNAME}.sif docker-daemon:jedi-${CNAME}:${TAG} 2>&1 | tee singularity_build.log
+
+    get_ans "Push Singularity image to S3 and backup existing version?"
+    if [[ $ans == y ]] ; then
+       aws s3 mv s3://privatecontainers/jedi-${CNAME}.sif s3://privatecontainers/jedi-${CNAME}-revert.tar.gz
+       aws s3 cp containers/jedi-${CNAME}.sif s3://privatecontainers/jedi-${CNAME}.sif
+    fi
 fi
 
 echo "=============================================================="
