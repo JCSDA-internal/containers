d33d44a992520950e024ca9cff382d0438d15856
mmiesch <miesch@ucar.edu>
2020-12-18 09:39:42 -0700

    split build and push to facilitate testing

--- build_public_container.sh
+++ build_public_container.sh
@@ -12,16 +12,15 @@ function get_ans {
       read ans < /dev/stdin
       if [[ $ans != y ]] && [[ $ans != n ]]; then echo "You must enter y or n"; fi
     done
-}    
+}
 
 #------------------------------------------------------------------------
-# This script creates a new Charliecloud container and optionally pushes it to Amazon S3
-# This is designed to use with our public containers that can just be pulled from 
-# Docker Hub, converted to a charliecloud container, and stored on data.jcsda.org 
+# This script creates new Singularity and Charliecloud containers from the
+# existing Docker containers we use for CI.
 
-if [ $# -ne 1 ]; then
+if [ $# -lt 1 ]; then
    echo "Usage: "
-   echo "./build_public_container.sh <container-name>"
+   echo "./build_public_container.sh <container-name> <tag>"
    exit 1
 fi
 
@@ -33,6 +32,7 @@ export USE_SUDO=${USE_SUDO:-"y"}
 [[ $USE_SUDO =~ [yYtT] ]] && export SUDO="sudo" || unset SUDO
 
 export CNAME=${1:-"gnu-openmpi-dev"}
+export TAG=${2:-"latest"}
 
 echo "=============================================================="
 echo "   Building Docker Image" ${CNAME}
@@ -41,11 +41,11 @@ echo "=============================================================="
 mkdir -p context
 cd context
 ln -sf ../Dockerfile.${CNAME} .
-$SUDO docker image build --no-cache --pull -t ch-${CNAME} -f Dockerfile.${CNAME} .
+$SUDO docker image build --no-cache --pull -t ch-${CNAME}:${TAG} -f Dockerfile.${CNAME} .
 cd ..
 
 echo "=============================================================="
-echo "   Building Charliecloud Image" ${CNAME}
+echo "   Building Charliecloud Image" ${CNAME}_${TAG}
 echo "=============================================================="
 
 get_ans "Build Charliecloud image?"
@@ -53,56 +53,27 @@ get_ans "Build Charliecloud image?"
 if [[ $ans == y ]] ; then
     echo "Building Charliecloud image"
     mkdir -p containers
-    $SUDO ch-builder2tar ch-${CNAME} containers
-
-    # Optionally copy to amazon S3
-    get_ans "Push Charliecloud image to S3?"
-    if [[ $ans == y ]] ; then
-	echo "Sending to Amazon S3" 
-        aws s3api head-object --bucket data.jcsda.org --key containers/ch-jedi-${CNAME}.tar.gz && file_exists=true
-        if [ ${file_exists} ]; then
-          echo "Saving previous container as revert"
-	  aws s3 mv s3://data.jcsda.org/containers/ch-jedi-${CNAME}.tar.gz s3://data.jcsda.org/containers/ch-jedi-${CNAME}-revert.tar.gz
-        fi
-	aws s3 cp containers/ch-${CNAME}.tar.gz s3://data.jcsda.org/containers/ch-jedi-${CNAME}.tar.gz
-    else
-	echo "Not sending to Amazon S3" 
-    fi
+    $SUDO ch-builder2tar ch-${CNAME}_${TAG} containers
 else
    echo "Not building Charliecloud image"
 fi
 
 echo "=============================================================="
-echo "   Building Singularity Image" ${CNAME}
+echo "   Building Singularity Image" ${CNAME}_${TAG}
 echo "=============================================================="
 
 get_ans "Build Singularity image?"
 
-if [[ $ans == y ]] ; then 
+if [[ $ans == y ]] ; then
 
    mkdir -p containers
    cd containers
 
-   # Optionally push to sylabs cloud
-   get_ans "Make backup image on sylabs cloud?"
-   if [[ $ans == y ]] ; then
-       echo "Creating backup image"
-       singularity pull --force library://jcsda/public/jedi-${CNAME}:latest
-       singularity push jedi-${CNAME}_latest.sif library://jcsda/public/jedi-${CNAME}:revert
-       rm jedi-${CNAME}_latest.sif
-   else
-       echo "Not making backup image on sylabs cloud"
-   fi
-
    echo "Building Singularity image"
-   $SUDO singularity build jedi-$CNAME.sif ../Singularity.${CNAME}
+   $SUDO singularity build jedi-$CNAME_${TAG}.sif ../Singularity.${CNAME}
 
-   get_ans "Push singularity image to sylabs cloud?"
-   if [[ $ans == y ]] ; then
-       echo "Pushing to sylabs cloud"
-       singularity sign jedi-$CNAME.sif
-       singularity push jedi-$CNAME.sif library://jcsda/public/jedi-$CNAME:latest
-   fi
-else
-   echo "Not building Singularity image"
+   singularity sign jedi-$CNAME_${TAG}.sif
+
 fi
+
+exit 0
---
+++ push_public_container.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+# © Copyright 2020-2020 UCAR
+# This software is licensed under the terms of the Apache Licence Version 2.0 which can be obtained at
+# http://www.apache.org/licenses/LICENSE-2.0.
+
+
+#------------------------------------------------------------------------
+function get_ans {
+    ans=''
+    while [[ $ans != y ]] && [[ $ans != n ]]; do
+      echo $1
+      read ans < /dev/stdin
+      if [[ $ans != y ]] && [[ $ans != n ]]; then echo "You must enter y or n"; fi
+    done
+}
+
+#------------------------------------------------------------------------
+# This script pushes Singularity and Charliecloud containers to Sylabs cloud
+# or AWS S3, optionally making a backup.
+
+if [ $# -ne 1 ]; then
+   echo "Usage: "
+   echo "./build_public_container.sh <container-name> <tag>"
+   exit 1
+fi
+
+# Stop if anything goes wrong
+set -e
+
+# may need to use sudo unless you're running as root
+export USE_SUDO=${USE_SUDO:-"y"}
+[[ $USE_SUDO =~ [yYtT] ]] && export SUDO="sudo" || unset SUDO
+
+export CNAME=${1:-"gnu-openmpi-dev"}
+export TAG=${$2:-"latest"}
+
+echo "=============================================================="
+echo "   Pushing Charliecloud Container" ${CNAME}_${TAG}
+echo "=============================================================="
+
+get_ans "Push Charliecloud container?"
+
+if [[ $ans == y ]] ; then
+	echo "Pushing Charliecloud container to Amazon S3"
+    if [[ ${TAG} == "latest" ]]; then
+        aws s3api head-object --bucket data.jcsda.org --key containers/ch-jedi-${CNAME}.tar.gz && file_exists=true
+        if [ ${file_exists} ]; then
+          echo "Saving previous container as revert"
+	      aws s3 mv s3://data.jcsda.org/containers/ch-jedi-${CNAME}.tar.gz s3://data.jcsda.org/containers/ch-jedi-${CNAME}-revert.tar.gz
+        fi
+    fi
+	aws s3 cp containers/ch-${CNAME}_${TAG}.tar.gz s3://data.jcsda.org/containers/ch-jedi-${CNAME}.tar.gz
+else
+   echo "Not pushing Charliecloud image"
+fi
+
+echo "=============================================================="
+echo "   Pushing Singularity Image" ${CNAME}
+echo "=============================================================="
+
+get_ans "Push Singularity image?"
+
+if [[ $ans == y ]] ; then
+
+  if [[ $(echo $CNAME| cut -d- -f1) == "intel" ]]; then
+
+    if [[ ${TAG} == "latest" ]]; then
+      aws s3api head-object --bucket data.jcsda.org --key containers/jedi-${CNAME}.sif && file_exists=true
+      if [ ${file_exists} ]; then
+        echo "Saving previous container as revert"
+	    aws s3 mv s3://data.jcsda.org/containers/jedi-${CNAME}.sif s3://data.jcsda.org/containers/jedi-${CNAME}-revert.sif
+      fi
+    fi
+	aws s3 cp containers/${CNAME}_${TAG}.sif s3://data.jcsda.org/containers/jedi-${CNAME}.tar.gz
+
+  else # push gnu and clang containers to sylabs cloud
+
+    echo "Pushing to sylabs cloud"
+
+    if [[ ${TAG} == "latest"]]; then
+      get_ans "Make backup image on sylabs cloud?"
+      if [[ $ans == y ]] ; then
+        echo "Creating backup image"
+        singularity pull --force library://jcsda/public/jedi-${CNAME}:latest
+        singularity push jedi-${CNAME}_latest.sif library://jcsda/public/jedi-${CNAME}:revert
+        rm jedi-${CNAME}_latest.sif
+      else
+        echo "Not making backup image on sylabs cloud"
+      fi
+    fi
+
+    singularity push jedi-$CNAME.sif library://jcsda/public/jedi-$CNAME:latest
+  fi
+else
+   echo "Not pushing Singularity image"
+fi
